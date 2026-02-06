d3e3f30dc3907af10bd027bd5419f9275866718b
Birhanu Asmamaw <birhanuasmamaw24@gmail.com>
2026-02-06 19:15:56 +0300

    feat: finalize asset registry and export helpers

---
+++ src/assets/asset.ts
@@ -0,0 +1,57 @@
+import award from '../assets/Award/award.png';
+import fire from '../assets/fire_disaster.jpg';
+import sourcing from '../assets/sourcing.jpg';
+import transportation from '../assets/transportation.jpg';
+import water from '../assets/water_swage.jpg';
+import advisory from '../assets/advisory.jpeg';
+import market from '../assets/market.jpg';
+import Business from '../assets/Business.png';
+import safety from '../assets/safety_security.jpg'
+import military from '../assets/military.jpg';
+import agreculture from '../assets/agreculture.jpg';
+import aviation from '../assets/aviation.jpg';
+import power from '../assets/power.png';
+import topfooter from '../assets/TopFooter.png';
+import import_export from '../assets/import_export.png';
+import rosenbauer from '../assets/global_images/rosenbauer.svg';
+import ASI from '../assets/global_images/ASI.png';
+import SACCON from '../assets/global_images/saccon.png';
+
+import sabollaLogo from '../assets/logo/sabolla_logo_icon.png';
+import worldMap from '../assets/world-map.png';
+import bgPattern from "../assets/bg_pattern/image.png";
+import map from '../assets/map.svg';
+import disasterRisk from '../assets/disasterRisk.png';
+import ethiopianToll from '../assets/ethiopianToll.png';
+import waterSwage from '../assets/waterSwage.png';
+import yiwu from '../assets/yiwu.png';
+
+export {
+    award,
+    fire,
+    sourcing,
+    transportation,
+    water,
+    advisory,
+    market,
+    Business,
+    safety,
+    military,
+    agreculture,
+    aviation,
+    power,
+    topfooter,
+    import_export,
+    rosenbauer,
+    ASI,
+    SACCON,
+    sabollaLogo,
+    worldMap,
+    bgPattern,
+    map,
+    disasterRisk,
+    ethiopianToll,
+    waterSwage,
+    yiwu
+
+}
